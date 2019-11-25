7d64ce412757ac1c3e33d871d5dffa335b447392
Vianney <vianneyjchin@gmail.com>
2019-11-25 15:56:41 -0800

    Add react-navigation

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { SafeAreaView, Text } from 'react-native'
 import { ThemeProvider } from 'styled-components'
 import { theme } from '../styles/theme'
 import { RecentMatchesScreen } from './RecentMatchesScreen'
