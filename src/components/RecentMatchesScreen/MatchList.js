1c760098b0047a1473d3341da0e1a3cd1f1a6180
Vianney <vianneyjchin@gmail.com>
2019-11-28 19:00:37 -0800

    Move the arrow function outside of the FlatList to improve performance

--- src/components/RecentMatchesScreen/MatchList.js
+++ src/components/RecentMatchesScreen/MatchList.js
@@ -4,27 +4,29 @@ import { MatchItem } from './MatchItem'
 import { mapUtil, gameModeUtil, gameResultUtil } from './util'
 
 export const MatchList = ({ data }) => {
+  const renderItem = ({ item }) => {
+    return (
+      <MatchItem
+        matchResult={gameResultUtil(item.result)}
+        gameMap={mapUtil(item.map)}
+        gameMode={gameModeUtil(item.mode)}
+        playerKills={item.playerStats.kills}
+        playerDeaths={item.playerStats.deaths}
+        playerAccuracy={item.playerStats.accuracy.toFixed(2)}
+        winOrLoseBG={
+          gameResultUtil(item.result) === 'W'
+            ? 'rgb(158, 144, 62)'
+            : 'rgb(51, 51, 51)'
+        }
+      />
+    )
+  }
+
   return (
     <FlatList
       data={data}
       keyExtractor={item => item.matchID}
-      renderItem={({ item }) => {
-        return (
-          <MatchItem
-            matchResult={gameResultUtil(item.result)}
-            gameMap={mapUtil(item.map)}
-            gameMode={gameModeUtil(item.mode)}
-            playerKills={item.playerStats.kills}
-            playerDeaths={item.playerStats.deaths}
-            playerAccuracy={item.playerStats.accuracy.toFixed(2)}
-            winOrLoseBG={
-              gameResultUtil(item.result) === 'W'
-                ? 'rgb(158, 144, 62)'
-                : 'rgb(51, 51, 51)'
-            }
-          />
-        )
-      }}
+      renderItem={renderItem}
     />
   )
 }
