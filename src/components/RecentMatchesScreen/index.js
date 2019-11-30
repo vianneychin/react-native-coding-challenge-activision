18f53e563709d8e15608fc70467604d0ccce0237
Vianney <vianneyjchin@gmail.com>
2019-11-30 11:06:14 -0800

    Remove unused variable

--- src/components/RecentMatchesScreen/index.js
+++ src/components/RecentMatchesScreen/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react'
 import { ActivityIndicator } from 'react-native'
 import { MatchList } from './MatchList'
 import { cookieUtil } from './util'
